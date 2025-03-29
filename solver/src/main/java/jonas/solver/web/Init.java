520312af5150040c072f18a9552691f159a43ac3
Jonas Friesen <jofri108@uni-duesseldorf.de>
2025-03-29 18:58:20 +0100

    added logo

---
+++ solver/src/main/java/jonas/solver/web/Init.java
@@ -0,0 +1,14 @@
+package jonas.solver.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Init {
+    
+    @GetMapping("/")
+    public String index(){
+        
+        return "hello";
+    }
+}
