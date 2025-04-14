9a4984199ee6d7ab1342c6c9f02d9e148ed2d53d
Jonas Friesen <jofri108@uni-duesseldorf.de>
2025-04-14 14:30:26 +0200

    first tests about accessing / and /main

---
+++ solver/src/test/java/jonas/solver/InitTest.java
@@ -0,0 +1,52 @@
+package jonas.solver;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.assertj.MockMvcTester.MockMvcRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class InitTest {
+    
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    @DisplayName("die Startseite ist unter / erreichbar, und zwar per redirect wegen spring security")
+    void test_1() throws Exception{
+        mvc.perform(get("/"))
+        .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    @WithMockUser(username="testuser", roles = {"USER", "ADMIN"})
+    @DisplayName("Eingeloggte User erreichen ebenfalls / per 200")
+    void test_2() throws Exception{
+        mvc.perform(get("/")).andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Nicht-eingeloggte user können nicht auf /main zugreifen und werden redirected")
+    void test_3() throws Exception{
+        mvc.perform(get("/main"))
+        .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    @WithMockUser(username = "testuser", roles = {"USER", "ADMIN"})
+    @DisplayName("Eingeloggte User erreichen /main")
+    void test_4() throws Exception{
+        mvc.perform(get("/main")).andExpect(status().isOk());
+    }
+
+}
