6ed064f2ea90ba094183b2164bbb62469098f78a
Jonas Friesen <jofri108@uni-duesseldorf.de>
2025-07-25 14:52:32 +0200

    highlighted current Position with actions

--- solver/src/main/java/jonas/solver/domain/service/GameService.java
+++ solver/src/main/java/jonas/solver/domain/service/GameService.java
@@ -2,34 +2,73 @@ package jonas.solver.domain.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
+
 import org.springframework.stereotype.Service;
 
+import jonas.solver.domain.model.Action;
 import jonas.solver.domain.model.Player;
 import jonas.solver.domain.model.Position;
 
 @Service
 public class GameService {
     private List<Player> allPlayers = new ArrayList<>();
-    // private final List<Position> positionen = new ArrayList<>();
+    private int currentPlayerIndex = 0;
     public GameService() {
         for(Position position: Position.values()){
             allPlayers.add(new Player(position, null));
         }
     }
 
-    // public Map<Position, Player> getPlayers() {
-    //     return players;
-    // }
-    
-    // public void updatePlayer(Position position, Action action, int stack){
-    //     Player player = players.get(position);
-    //     if(player != null){
-    //         player.setAction(action);
-    //         player.setStackSize(stack);
-    //     }
-    // }
-
     public List<Player> getPlayers(){
         return allPlayers;
+    }
+
+    public Player getPlayerAtI(int index){
+        return allPlayers.get(index);
+    }
+
+    public Player getCurrentPlayer(){
+        return allPlayers.get(currentPlayerIndex);
+    }
+
+    public List<Position> getPositions(){
+        Position[] positions = Position.values();
+        Vector<Position> allPositions = new Vector<>();
+        for(Position p: positions){
+            allPositions.addLast(p);
+        }
+        return allPositions;
+    }
+
+    public void resetActions(){
+        for(Player p: allPlayers){
+            p.setAction(null);
+        }
+        currentPlayerIndex = 0;
+    }
+
+    public void processAction(String positionName, String action){
+        // handle out of bounds exception after action of BB
+        // handle next Actions after BB done and Flop coming
+
+        Position position = Position.valueOf(positionName.toUpperCase());
+        int selectedIndex = -1;
+
+        for(int i = 0; i < allPlayers.size(); i++){
+            Player p = allPlayers.get(i);
+            if(p.getPosition() == position){
+                selectedIndex = i;
+                p.setAction(Action.valueOf(action.toUpperCase()));
+            }
+        }
+        for(int i = 0; i < selectedIndex; i++){
+            if(allPlayers.get(i).getAction() == null){
+                currentPlayerIndex++;
+                allPlayers.get(i).setAction(Action.FOLD);
+            }
+        }
+        currentPlayerIndex++;
+        if(currentPlayerIndex >= allPlayers.size()) currentPlayerIndex--; // temp fix
     }
 }
--- solver/src/main/java/jonas/solver/web/PokerController.java
+++ solver/src/main/java/jonas/solver/web/PokerController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import jonas.solver.domain.model.Action;
 import jonas.solver.domain.model.Player;
 import jonas.solver.domain.model.Position;
 import jonas.solver.domain.service.GameService;
@@ -26,7 +25,7 @@ public class PokerController {
     }
 
     @ModelAttribute("allPlayers")
-    public List<Player> allPositions(){
+    public List<Player> allPlayers(){
         return gameService.getPlayers();
     }
 
@@ -43,14 +42,17 @@ public class PokerController {
         return "profile";
     }
 
-    @GetMapping("/main")
-    public String main(Model model){
-        model.addAttribute("format", "");
-        model.addAttribute("solution", "");
-        model.addAttribute("players", 0);
+@GetMapping("/main")
+public String main(Model model){
+    model.addAttribute("format", "");
+    model.addAttribute("solution", "");
+    model.addAttribute("players", 0);
+    model.addAttribute("currentPlayer", gameService.getCurrentPlayer());
+    model.addAttribute("allPlayers", gameService.getPlayers());
 
-        return "main";
-    }
+    return "main";
+}
+
 
     @GetMapping("/table/format")
     public String changeFormat(@RequestParam(value = "format", required = false) String format,
@@ -66,24 +68,20 @@ public class PokerController {
     }
 
     @PostMapping("/main/action")
-    public String makeAction(@ModelAttribute("allPlayers") List<Player> allPlayers, @RequestParam("action") String action,
+    public String makeAction(@RequestParam("action") String action,
     @RequestParam("stack") int stack, @RequestParam("position") String positionName, Model model){
-        Position position = Position.valueOf(positionName.toUpperCase());
-        int selectedIndex = -1;
 
-        for(int i = 0; i < allPlayers.size(); i++){
-            Player p = allPlayers.get(i);
-            if(p.getPosition() == position){
-                selectedIndex = i;
-                p.setAction(Action.valueOf(action.toUpperCase()));
-            }
-        }
-        for(int i = 0; i < selectedIndex; i++){
-            if(allPlayers.get(i).getAction() == null){
-                allPlayers.get(i).setAction(Action.FOLD);
-            }
-        }
-        model.addAttribute("allPlayers", allPlayers);
+        gameService.processAction(positionName, action);
+
+        model.addAttribute("allPlayers", gameService.getPlayers());
+        model.addAttribute("currentPlayer", gameService.getCurrentPlayer());
+
+        return "main";
+    }
+
+    @PostMapping("/main/reset")
+    public String resetActions(){
+        gameService.resetActions();
 
         return "main";
     }
