d2c8ef605f6aa69574d5037634c232fc0704e4c2
Jonas Friesen <jofri108@uni-duesseldorf.de>
2025-07-28 14:46:24 +0200

    added tailwind config file to gitignore

--- solver/src/main/frontend/tailwind.config.js
+++ solver/src/main/frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  content: ["../resources/templates/**/*.{html,js}"],
   theme: {
     extend: {},
   },
