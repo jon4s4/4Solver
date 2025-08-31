32ce078192ea4a032005ebccbd554785adc03905
Jonas Friesen <jofri108@uni-duesseldorf.de>
2025-08-31 10:00:43 +0200

    worked on table, still null data in template

--- solver/src/main/java/jonas/solver/web/RangesController.java
+++ solver/src/main/java/jonas/solver/web/RangesController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import jonas.solver.application.HandRange;
@@ -18,15 +19,18 @@ public class RangesController {
     public RangesController(RangeService rangeService) {
         this.rangeService = rangeService;
     }
+
+    @ModelAttribute("ranks")
+    public List<String> getRanks(){
+        List<String> ranks = List.of("A","K","Q","J","T","9","8","7","6","5","4","3","2");
+
+        return ranks;
+    }
     
     @GetMapping("ranges")
     public String rangesInit(Model model){
-        List<String> ranks = List.of("A","K","Q","J","T","9","8","7","6","5","4","3","2");
         Map<String, HandRange> hands = rangeService.loadRanges("utg_vs_bb.csv");
         
-        hands.values().stream().forEach(hr -> System.out.println(hr.getAction()));
-        
-        model.addAttribute("ranks",ranks);
         model.addAttribute("hands", hands);
         model.addAttribute("matchup", "utg_vs_bb");
 
@@ -35,10 +39,9 @@ public class RangesController {
 
     @GetMapping("/ranges/{filename}")
     public String showRanges(Model model, @PathVariable("filename") String filename){
-        Map<String, HandRange> ranges = rangeService.loadRanges(filename + ".csv"); 
-        model.addAttribute("ranges", ranges);
-        model.addAttribute("scenario", filename);
-        model.addAttribute("matchup", "filename");
+        Map<String, HandRange> hands = rangeService.loadRanges(filename + ".csv");
+        model.addAttribute("scenario", hands);
+        model.addAttribute("matchup", filename);
         return "ranges";
     }
 }
